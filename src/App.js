3d3a74d9ea5f887719f95ca206122405692c73d6
annapaula20 <annapaula.desouza1@gmail.com>
2023-12-29 14:53:13 -0500

    creating find doctor search component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
 import InstantConsultationBooking from './Components/InstantConsultationBooking/InstantConsultation';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
                     <Route path='/Sign_Up' element={<Sign_Up/>}></Route>
                     <Route path='/Login' element={<Login/>}></Route>
                     <Route path='/instant-consultation' element={<InstantConsultationBooking />} />
+                    <Route path='/finddoctor' element={<FindDoctorSearch />} />
                 </Routes>
             </BrowserRouter>
 
